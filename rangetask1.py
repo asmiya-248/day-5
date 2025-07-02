b5ccd2dedd1b21798d1348d69f499a3fa02f7b65
unknown <asmiya.s248@gmail.com>
2025-07-02 15:56:13 +0530

    rangepractice1

---
+++ rangetask1.py
@@ -0,0 +1,13 @@
+#single parameter
+q=range(20)
+print(q)
+print(type(q))
+# 2 parameters
+r=range(4,40)
+print(r)
+print(type(r))
+#3 parameters
+s=range(1,50,4)
+print(s)
+print(type(s))
+ 
---
+++ rangetask2.py
@@ -0,0 +1,12 @@
+
+t=range(3)
+print(t)
+print(type(t))
+
+u=range(35,45)
+print(u)
+print(type(u))
+
+v=range(5, 0, -1)
+print(v)
+print(type(v))
