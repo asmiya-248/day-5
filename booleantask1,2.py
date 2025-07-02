9c844d1e034c21c4949752561317140d1fe88cad
unknown <asmiya.s248@gmail.com>
2025-07-02 16:32:10 +0530

    boolean tasks 1,2,3

---
+++ booleantask1,2.py
@@ -0,0 +1,9 @@
+a=True,True
+print(a)
+print(type(a))
+ 
+
+a=100
+b=50
+print("a is greater than b")
+print("a is not grater than b")
---
+++ booleantask3.py
@@ -0,0 +1,5 @@
+d1={"a":1}
+d2={}
+print(d1 and d2)
+print(d1 or d2)
+print(not d2)
